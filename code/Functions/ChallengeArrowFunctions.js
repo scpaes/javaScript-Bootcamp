1e98d96579ff0ff410b3df9805cd3165e4b1aa6e
scpaes <scp2@cin.ufpe.br>
2022-04-29 14:10:43 -0300

    update project

--- code/Functions/ChallengeArrowFunctions.js
+++ code/Functions/ChallengeArrowFunctions.js
@@ -3,24 +3,35 @@
 
 
 // Challenge start 
-function splitBill(amount, numPeople) {
-    return `Each person needs to pay ${amount / numPeople}`
-}
+// function splitBill(amount, numPeople) {
+//     return `Each person needs to pay ${amount / numPeople}`
+// }
 
-// console.log(splitBill(10, 2));
-// console.log(splitBill(10, 4));
-// console.log(splitBill(10, 5));
+const splitBill = (amount, numPeople) => `Each person needs to pay ${amount / numPeople}`;
+
+console.log(splitBill(10, 2));
+console.log(splitBill(10, 4));
+console.log(splitBill(10, 5));
 
 
 // Stretch goal start
-function countdown(startingNumber, step) {
+function countdown(startingNumber, step) 
+{
   let countFromNum = startingNumber + step;
-  return function decrease() {
+  return function decrease()
+  {
     countFromNum -= step;
     return countFromNum;
   }
 }
 
+const countdown = (startingNumber, step) => {
+    let countFromNum = startingNumber + step;
+    return () => {
+        countFromNum -= step;
+        return (countFromNum)
+    }
+}
 // const countingDown = countdown(20, 2);
 
 // console.log(countingDown());
